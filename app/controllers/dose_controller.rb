6a7633e3f82dd194c6d99d0161b13de559f1a029
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-16 23:31:52 +0100

    Add controller dose

---
+++ app/controllers/dose_controller.rb
@@ -0,0 +1,22 @@
+class DoseController < ApplicationController
+  def new
+    @dose=Dose.new
+    @ingredients = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "cocktails#index"
-  resources :cocktails
+  resources :cocktails do
+    resources :dose, only: [ :new, :create ]
 
-
-
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
