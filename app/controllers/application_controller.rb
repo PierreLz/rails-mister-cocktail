5220b369b63cc5d5b673efd0714c2db4a7a8d4f0
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-18 16:10:32 +0100

    Add method in app controller to disable nav_bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def disable_nav
+    @disable_nav = true
+  end
 end
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_filter :disable_nav, only: [:home]
   def home
   end
 
